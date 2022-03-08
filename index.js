3260ca49920ff9ab2c1b8a1cb56ee0fab4ddb74e
ennichi-yamasaki <yamasaki@ennichi.biz>
2022-03-08 19:20:48 +0900

    first commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ $(function () {
         var number = $('input[name="number"]:checked').val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
+            names += $(elm).val() + '様、';
         })
         names = names.slice(0, -1);
 
